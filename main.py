b565c374c21cc67487f8e2b2a81caf4006b6ee81
shrutisharma91 <www.7425shruti@gmail.com>
2025-05-04 12:22:56 +0530

    deployment changes #6

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.include_router(predict_router)
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Render's dynamic port
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 
 
